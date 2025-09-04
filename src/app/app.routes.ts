7a1a36c5bc20cb98b1d4a12d564eda2768a2c890
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-09-04 17:43:08 +0300

    Enabling router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,67 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
+import { AuthService } from './sevrice/auth-service';
 
-export const routes: Routes = [];
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  return authService.isAuthenticated();
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async () => {
+      const m = await import('./pages/home/home');
+      return m.Home;
+    },
+  },
+  {
+    path: 'teachers',
+    canActivate: [authGuard],
+    loadComponent: async () => {
+      const m = await import('./pages/teachers/teachers');
+      return m.Teachers;
+    },
+    children: [
+      {
+        path: 'new',
+        loadComponent: async () => {
+          const m = await import('./components/form/form');
+          return m.Form;
+        },
+      },
+      {
+        path: ':id',
+        loadComponent: async () => {
+          const m = await import('./components/form/form');
+          return m.Form;
+        },
+      },
+    ],
+  },
+  {
+    path: 'groups',
+    canActivate: [authGuard],
+    loadComponent: async () => {
+      const m = await import('./pages/groups/groups');
+      return m.Groups;
+    },
+  },
+  {
+    path: 'workloads',
+    canActivate: [authGuard],
+    loadComponent: async () => {
+      const m = await import('./pages/workloads/workloads');
+      return m.Workloads;
+    },
+  },
+];
