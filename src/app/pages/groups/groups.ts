995725284d237d0b099f4026da96db41c6e4d0c1
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-09-08 02:10:29 +0300

    Group refactoring

--- src/app/pages/groups/groups.ts
+++ src/app/pages/groups/groups.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Table } from '../../components/table/table';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-groups',
