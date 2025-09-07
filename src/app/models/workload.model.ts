918619512ac536fabf88ebbc228a3c921ad73d48
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-09-08 02:07:24 +0300

    Workload model updated

--- src/app/models/workload.model.ts
+++ src/app/models/workload.model.ts
@@ -1,9 +1,24 @@
+import { Group } from './group.model';
+import { LessonType } from './lesson-type.model';
+import { Teacher } from './teacher.model';
+import { Subject } from './subject.model';
+
 export type Workload = {
-  _id?: string;
+  _id: string;
+  teacher: Teacher;
+  group: Group;
+  subject: Subject;
+  type: LessonType;
+  hours: number;
+  price: number;
+};
+
+export type WorkloadTableRow = {
+  _id: string;
   teacher: string;
   group: string;
   subject: string;
   type: string;
   hours: number;
-  price: number;
+  price: string;
 };
